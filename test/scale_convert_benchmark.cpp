60ff6df5c41c664025d34fef8d7c9fc08511d377
NatalyaRostova <347563303@qq.com>
2023-03-29 11:53:01 +0800

    revise benchmark

--- test/scale_convert_benchmark.cpp
+++ test/scale_convert_benchmark.cpp
@@ -1,17 +1,9 @@
 #include <libyuv.h>
 
 #include <chrono>
-#include <deque>
-#include <fstream>
+#include <cmath>
 #include <functional>
 #include <thread>
-
-#include "logger.h"
-#include "media_decoder_common.h"
-#include "media_decoder_interface.h"
-#include "media_recorder_common.h"
-#include "media_recorder_interface.h"
-#include "ring_fifo.h"
 
 extern "C" {
 #include <libavcodec/avcodec.h>
@@ -75,30 +67,17 @@ void FFmpegRGB24ToYUV420P(uint8_t *src, int src_width, int src_height, uint8_t *
     SwsContext *sws_ctx =
         sws_getContext(src_width, src_height, AV_PIX_FMT_RGB24, dst_width, dst_height,
                        AV_PIX_FMT_YUV420P, SWS_FAST_BILINEAR, 0, 0, 0);
-    AVFrame *src_frame;
-    src_frame = av_frame_alloc();
-    src_frame->width = src_width;
-    src_frame->height = src_height;
-    src_frame->format = AV_PIX_FMT_RGB24;
-    AVFrame *dst_frame;
-    av_frame_get_buffer(src_frame, 0);
 
-    dst_frame = av_frame_alloc();
-    dst_frame->width = dst_width;
-    dst_frame->height = dst_height;
-    dst_frame->format = AV_PIX_FMT_YUV420P;
-    av_frame_get_buffer(dst_frame, 0);
+    uint8_t *srcSlice[1] = {src};
+    int srcStride[1] = {src_width * 3};
 
-    memcpy(src_frame->data[0], src, src_width * src_height * 3);
-    sws_scale(sws_ctx, src_frame->data, src_frame->linesize, 0, src_frame->height,
-              dst_frame->data, dst_frame->linesize);
-    memcpy(dst, dst_frame->data[0], dst_width * dst_height);
-    memcpy(dst + dst_width * dst_height, dst_frame->data[0],
-           (dst_width >> 1) * (dst_height >> 1));
-    memcpy(dst + dst_width * dst_height + (dst_width >> 1) * (dst_height >> 1),
-           dst_frame->data[0], (dst_width >> 1) * (dst_height >> 1));
-    av_frame_free(&src_frame);
-    av_frame_free(&dst_frame);
+    uint8_t *dstSlice[3] = {
+        dst, dst + dst_width * dst_height,
+        dst + dst_width * dst_height + (dst_height >> 1) * (dst_width >> 1)};
+    int dstStride[3] = {dst_width, dst_width >> 1, dst_width >> 1};
+
+    sws_scale(sws_ctx, srcSlice, srcStride, 0, src_height, dstSlice, dstStride);
+
     sws_freeContext(sws_ctx);
 }
 
@@ -107,49 +86,25 @@ void FFmpegYUV420PToRGB24(uint8_t *src, int src_width, int src_height, uint8_t *
     SwsContext *sws_ctx =
         sws_getContext(src_width, src_height, AV_PIX_FMT_YUV420P, dst_width, dst_height,
                        AV_PIX_FMT_RGB24, SWS_FAST_BILINEAR, 0, 0, 0);
-    AVFrame *src_frame;
-    src_frame = av_frame_alloc();
-    src_frame->width = src_width;
-    src_frame->height = src_height;
-    src_frame->format = AV_PIX_FMT_YUV420P;
-    AVFrame *dst_frame;
-    av_frame_get_buffer(src_frame, 0);
 
-    dst_frame = av_frame_alloc();
-    dst_frame->width = dst_width;
-    dst_frame->height = dst_height;
-    dst_frame->format = AV_PIX_FMT_RGB24;
-    av_frame_get_buffer(dst_frame, 0);
+    uint8_t *dstSlice[1] = {dst};
+    int dstStride[1] = {dst_width * 3};
 
-    memcpy(src_frame->data[0], src, src_width * src_height);
-    memcpy(src_frame->data[1], src + src_width * src_height,
-           (src_width >> 1) * (src_height >> 1));
-    memcpy(src_frame->data[2],
-           src + src_width * src_height + (src_width >> 1) * (src_height >> 1),
-           (src_width >> 1) * (src_height >> 1));
-    sws_scale(sws_ctx, src_frame->data, src_frame->linesize, 0, src_frame->height,
-              dst_frame->data, dst_frame->linesize);
-    memcpy(dst, dst_frame->data[0], dst_width * dst_height * 3);
-    av_frame_free(&src_frame);
-    av_frame_free(&dst_frame);
+    uint8_t *srcSlice[3] = {
+        src, src + src_width * src_height,
+        src + src_width * src_height + (src_height >> 1) * (src_width >> 1)};
+    int srcStride[3] = {src_width, src_width >> 1, src_width >> 1};
+
+    sws_scale(sws_ctx, srcSlice, srcStride, 0, src_height, dstSlice, dstStride);
+
     sws_freeContext(sws_ctx);
 }
 
 int main(int argc, char **argv) {
-    if (argc < 2) {
-        printf("usage %s input_file\n", argv[0]);
-        exit(-1);
-    }
-    MediaDecoder *dec = MediaDecoder::CreateVideoDecoder();
-    VideoDecoderStartParam dec_param;
-    dec_param.filename = argv[1];
-    MediaDecoderStartRet dec_ret = dec->Start(&dec_param);
+    int width = 1920;
+    int height = 1080;
 
-    AVFrame *frame = nullptr;
-    dec->InitFrame(&frame);
-    dec->ReadFrame(frame);
-
-    uint8_t *src = new uint8_t[dec_ret.width * dec_ret.height * 4];
+    uint8_t *src = new uint8_t[width * height * 3];
     uint8_t *dst = new uint8_t[3840 * 2160 * 4];
 
     std::function<void(int, int, BenchmarkFunction, std::string)> benchMark =
@@ -160,7 +115,7 @@ int main(int argc, char **argv) {
                              .count();
 
             for (int i = 0; i < 1000; i++) {
-                function(src, dec_ret.width, dec_ret.height, dst, dst_width, dst_height);
+                function(src, width, height, dst, dst_width, dst_height);
             }
 
             int64_t t2 = std::chrono::duration_cast<std::chrono::milliseconds>(
@@ -170,29 +125,76 @@ int main(int argc, char **argv) {
                 "\033[1;33mFunction\033[0m [\033[0;32;34m%s\033[0m] from "
                 "[\033[0;32m%dx%d\033[0m] to [\033[0;32m%dx%d\033[0m] used "
                 "\033[0;36m%.3lfs\033[0m\n",
-                func_name.c_str(), dec_ret.width, dec_ret.height, dst_width, dst_height,
+                func_name.c_str(), width, height, dst_width, dst_height,
                 (t2 - t1) / 1000.0);
         };
 #define RunBenchMark(dw, dh, f) benchMark(dw, dh, f, #f);
 
-    // init rgb24 src buffer
-    memcpy(src, frame->data[0], frame->width * frame->height * 3);
+    // init rgb24 buffer
+    for (int i = 0; i < width * height; i++) {
+        int x = i % width;
+        int y = i / width;
+        if (x < width / 2) {
+            if (y < height / 2) {
+                src[i * 3 + 0] = 12;
+                src[i * 3 + 1] = 233;
+                src[i * 3 + 2] = 12;
+            } else {
+                src[i * 3 + 0] = 233;
+                src[i * 3 + 1] = 12;
+                src[i * 3 + 2] = 12;
+            }
+        } else if (x) {
+            if (y < height / 2) {
+                src[i * 3 + 0] = 233;
+                src[i * 3 + 1] = 12;
+                src[i * 3 + 2] = 233;
+            } else {
+                src[i * 3 + 0] = 12;
+                src[i * 3 + 1] = 233;
+                src[i * 3 + 2] = 233;
+            }
+        }
+    }
 
-    RunBenchMark(dec_ret.width, dec_ret.height, LibyuvRawToI420);
-    RunBenchMark(dec_ret.width, dec_ret.height, FFmpegRGB24ToYUV420P);
-    RunBenchMark(dec_ret.width / 4, dec_ret.height / 4, LibyuvRawToI420);
-    RunBenchMark(dec_ret.width / 4, dec_ret.height / 4, FFmpegRGB24ToYUV420P);
+    // RGB24 To YUV420P
+    RunBenchMark(width, height, LibyuvRawToI420);
+    RunBenchMark(width, height, FFmpegRGB24ToYUV420P);
+    RunBenchMark(width / 4, height / 4, LibyuvRawToI420);
+    RunBenchMark(width / 4, height / 4, FFmpegRGB24ToYUV420P);
 
-    // init yuv420p src buffer
-    libyuv::RAWToI420(
-        frame->data[0], frame->linesize[0], src, frame->width,
-        src + frame->width * frame->height, frame->width >> 1,
-        src + frame->width * frame->height + (frame->width / 2) * (frame->height / 2),
-        frame->width >> 1, frame->width, frame->height);
+    // init yuv420p buffer
+    for (int i = 0; i < width * height; i++) {
+        int x = i % width;
+        int y = i / width;
+        if (x < width / 2) {
+            if (y < height / 2) {
+                src[i] = 25;
+            } else {
+                src[i] = 178;
+            }
+        } else if (x) {
+            if (y < height / 2) {
+                src[i] = 234;
+            } else {
+                src[i] = 75;
+            }
+        }
+    }
+    for (int i = 0; i < (width >> 1); ++i) {
+        for (int j = 0; j < (height >> 1); ++j) {
+            // u
+            src[width * height + j * (width >> 1) + i] = 254 * i / (width >> 1);
+            // // v
+            src[width * height + (width >> 1) * (height >> 1) + j * (width >> 1) + i] =
+                254 * j / (height >> 1);
+        }
+    }
 
-    RunBenchMark(dec_ret.width, dec_ret.height, LibyuvI420ToRaw);
-    RunBenchMark(dec_ret.width, dec_ret.height, FFmpegYUV420PToRGB24);
-    RunBenchMark(dec_ret.width / 4, dec_ret.height / 4, LibyuvI420ToRaw);
-    RunBenchMark(dec_ret.width / 4, dec_ret.height / 4, FFmpegYUV420PToRGB24);
+    // YUV420P To RGB24
+    RunBenchMark(width, height, LibyuvI420ToRaw);
+    RunBenchMark(width, height, FFmpegYUV420PToRGB24);
+    RunBenchMark(width / 4, height / 4, LibyuvI420ToRaw);
+    RunBenchMark(width / 4, height / 4, FFmpegYUV420PToRGB24);
     return 0;
 }
